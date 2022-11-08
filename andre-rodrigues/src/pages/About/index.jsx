305e3e5ad4d20a38b6bc179734d1fa5c01e36834
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2022-11-08 06:45:58 -0300

    feat: update de caminho de imagens

--- andre-rodrigues/src/pages/About/index.jsx
+++ andre-rodrigues/src/pages/About/index.jsx
@@ -6,6 +6,7 @@ import { HeadProvider, Title, Link as LinkHead, Meta } from "react-head";
 //Styled Components
 import { PageContainer } from "../../components/Theme/style";
 import { ButtonLink } from "../../components/Buttons/style";
+import { CardProfile } from "./style";
 //MUI Componentes
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -19,6 +20,9 @@ import {
   Grid
 } from "@mui/material";
 
+//Base url midias
+const baseUrlmidias = "/assets/imgs/";
+
 export const About = () => {
   return (
     <HeadProvider>
@@ -28,13 +32,32 @@ export const About = () => {
         <Meta name="example" content="whatever" />
         <Container>
           <Grid container spacing={2} mt={2}>
+            <Grid item xs={12}>
+              <CardProfile>
+                <h2>Olá</h2>
+                <p>
+                  Meu nome é André Laurentino Rodrigues, tenho 29 anos, sou
+                  natural da cidade de Luz localizada em Minas Gerais, mas
+                  atualmente estou morando em Bambuí. Trabalho como
+                  Desenvolvimento Web desde 2012. Sou apaixonado por projetos
+                  open source, acredito que a disseminação de conhecimento e
+                  boas idéias inspira e melhora o mundo. Atualmente estou
+                  trabalhando como Desenvolvedor Front-End na NTT Data. Sou
+                  criador do framework Kanai, já ministrei diversos minicursos
+                  no IFMG Bambuí, além de diversas palestras focadas no
+                  ecossistema Front-End. Nos meus tempos livres amo joga no meu
+                  PC, assistir filmes, animes, além de ler livros sobre
+                  tecnologia, ecologia, folclore e cultura geek.
+                </p>
+              </CardProfile>
+            </Grid>
             <Grid item xs={4}>
               <Card sx={{ bgcolor: "#000", color: "#fff", maxWidth: 345 }}>
                 <CardActionArea>
                   <CardMedia
                     component="img"
                     height="140"
-                    image="../assets/imgs/code.jpg"
+                    image={baseUrlmidias + "code.jpg"}
                     alt="green iguana"
                   />
                   <CardContent>
@@ -50,7 +73,7 @@ export const About = () => {
                 </CardActionArea>
                 <CardActions>
                   <Link to="/home">
-                    <ButtonLink>Visualizar</ButtonLink>
+                    <ButtonLink>Confira meus portfólios</ButtonLink>
                   </Link>
                 </CardActions>
               </Card>
---
+++ andre-rodrigues/src/pages/About/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const CardProfile = styled.div`
+  background-color: #000;
+  border-radius: 4px;
+  box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%),
+    0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
+  color: #fff;
+  padding: 1em;
+  h2 {
+    font-size: 2rem;
+  }
+`;
+
+export { CardProfile };
