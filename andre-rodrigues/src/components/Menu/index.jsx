9340a88e3a10bf378e2b2f79d603116289d24967
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2022-11-08 04:37:40 -0300

    Feat: correção acentuação logo no menu

--- andre-rodrigues/src/components/Menu/index.jsx
+++ andre-rodrigues/src/components/Menu/index.jsx
@@ -12,7 +12,7 @@ export const Menu = () => {
                     <div className="logo">
                         <img src="https://scontent-gru2-1.xx.fbcdn.net/v/t39.30808-6/311869874_1231793387550094_7993381859158514975_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=eczbDVyyjEAAX-TuC8k&tn=dUEarFvQaL0t3P-B&_nc_ht=scontent-gru2-1.xx&oh=00_AfDvMW2tOZkpX97S-VslP6A9MgCQPHc9k6_cUOtAfKMClg&oe=636F4123" alt="Andre-Rodrigues" />
                         <Link to="/">
-                            <h1>André laurentino</h1>
+                            <h1>AndrÃ© laurentino</h1>
                         </Link>
                     </div>
                     <ul className="links">
